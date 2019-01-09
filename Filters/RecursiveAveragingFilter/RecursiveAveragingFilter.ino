18d041642f3155261e8df280ff9a211f988bf124
HoHo <houshumao@qq.com>
2019-01-09 20:16:08 +0800

    RecursiveAveragingFilter 递推平均滤波器（滑动平均滤波器）

---
+++ Filters/RecursiveAveragingFilter/RecursiveAveragingFilter.ino
@@ -0,0 +1,31 @@
+int Filter_Value;
+
+void setup() {
+  Serial.begin(9600);// 初始化串口通信
+  randomSeed(analogRead(0)); // 产生随机种子
+}
+
+void loop() {
+  Filter_Value = Filter();// 获得滤波器输出值
+  Serial.println(Filter_Value); // 串口输出
+  delay(50);
+}
+
+// 用于随机产生一个300左右的当前值
+int Get_AD() {
+  return random(295, 305);
+}
+
+// 递推平均滤波法（又称滑动平均滤波法）
+#define FILTER_N 12
+int filter_buf[FILTER_N + 1];
+int Filter() {
+  int i;
+  int filter_sum = 0;
+  filter_buf[FILTER_N] = Get_AD();
+  for (i = 0; i < FILTER_N; i++) {
+    filter_buf[i] = filter_buf[i + 1]; // 所有数据左移，低位仍掉
+    filter_sum += filter_buf[i];
+  }
+  return (int)(filter_sum / FILTER_N);
+}
